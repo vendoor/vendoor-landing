738341ab14780e12928f1b183e0bc95095aac1bd
Kovács Lajos <kovacs.lajos1218@gmail.com>
2020-04-03 00:19:42 +0200

    Initial commit for landing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+#app
+  .card
+    img.logo(alt="Vue logo" src="./assets/VendoorFULL.png")
+    .block.text-center.mt-12
+      h1.text-3xl.font-bold Coming soon...
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+#app
+  @apply flex justify-center items-center
+  font-family 'Montserrat', sans-serif
+  font-size 16px
+  height 100vh
+  width 100vw
+  background-color #D2DBDD
+.card
+  @apply p-12
+  background-color #FFF
+  border-radius 15px
+  box-shadow 0px 13px 10px -7px rgba(0, 0, 0,0.1)
+  transition all .4s cubic-bezier(0.175, 0.885, 0, 1)
+  &:hover
+    box-shadow 0px 30px 18px -8px rgba(0, 0, 0,0.1)
+    transform scale(1.10, 1.10)
+
+  .logo
+    width 25rem
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './style/tailwind.css'
+import './style/fonts/Montserrat/Montserrat.css'
+
 Vue.config.productionTip = false
 
 new Vue({
